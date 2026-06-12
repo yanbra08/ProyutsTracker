f78ffce58a37a7388d13b8da9bcd79386bc667d6
unknown <vacano300@gmail.com>
2026-06-12 01:04:31 -0500

    Fix package json scripts and sync

--- Backend/db.js
+++ Backend/db.js
@@ -1,16 +1,18 @@
 const { Pool } = require('pg');
 
-// Cadena de conexión externa directa a tu base de datos de Render
-const connectionString = process.env.DATABASE_URL || 'postgresql://proyuts_tracker_user:3dj7UEAgolt9pcWWNqFqGw7Q21LcR7Uq@dpg-d815bk67r5hc7391gnvg-a.oregon-postgres.render.com/proyuts_tracker';
-
 const pool = new Pool({
-  connectionString: connectionString,
-  // Si estás en tu PC (localhost) apaga SSL, en Render lo activa automáticamente
-  ssl: connectionString.includes('localhost') ? false : { rejectUnauthorized: false }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false // Obligatorio para que Render te deje conectar externamente
+  }
 });
 
 pool.on('connect', () => {
-  console.log('¡Conectado exitosamente a la base de datos PostgreSQL en la nube!');
+  console.log('¡Conectado exitosamente a la base de datos PostgreSQL!');
 });
 
 module.exports = pool;
